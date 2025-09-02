2111aef38b7b78a335f6ab40e742de6316f02b71
maxim <bozzhik@gmail.com>
2025-09-03 00:11:26 +0300

    init `Typography` UI dynamic component

--- src/components/App/index/Hero.tsx
+++ src/components/App/index/Hero.tsx
@@ -1,17 +1,19 @@
 import {cn} from '@/lib/utils'
 
+import {H1, P} from '~/UI/Typography'
+
 export default function Hero() {
   return (
     <section data-section="hero-index" className={cn('relative h-screen', 'grid place-items-center')}>
       <div className="space-y-8 xl:space-y-7 sm:space-y-6 text-center">
         <div className="space-y-6 xl:space-y-5 text-background">
-          <h1 className={cn('text-7xl xl:text-6xl sm:text-4xl font-normal sm:font-medium font-sans leading-[0.95] sm:leading-[1.1] tracking-tighter')}>
+          <H1>
             Советы специалистов, <br className="sm:hidden" /> которым можно доверять
-          </h1>
+          </H1>
 
-          <p className="text-base sm:text-sm tracking-tight">
+          <P>
             Выбирайте лучшее из подборок специалистов <br className="hidden sm:block" /> — мы привезём без хлопот.
-          </p>
+          </P>
         </div>
 
         <button className={cn('px-10 sm:px-8 py-3 sm:py-2.5 cursor-pointer', 'text-sm sm:text-xs text-background tracking-tight', 'bg-background/15 backdrop-blur-[2px] sm:backdrop-blur-[4px] border border-background/50 rounded-md', 'hover:bg-background/25 hover:border-background/60 duration-300')}>Лучшие подборки</button>
